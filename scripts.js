05d9b6be50f154b007c73d3a4f180388f264a7ab
alkagado <38248590+alkagado@users.noreply.github.com>
2018-04-16 19:42:42 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var a = 3;
+var h = 8;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+alert('Hello, ' + name);
+
+console.log('Hello, ' + name);
+
+// nauka nowego jêzyka
+
+// tralalalla
